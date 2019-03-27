60e1b99d988a7bfe5e6221a617ea67e77595b6ba
Soteded <theo.vicente@ynov.com>
2019-03-27 14:43:03 +0100

    contact

--- base_website_template/app.js
+++ base_website_template/app.js
@@ -1,10 +1,24 @@
 jQuery(function($){
-      $( window ).load(function() {
-        $(".preloader").fadeOut("slow");
-      });
- });
+  $(window).load(function() {
+    $(".preloader").fadeOut("slow");
+  });
+});
 
-
-$('.devAid').click(()=>{
-  $('.textAid').slideToggle();
+$("#choose-contact").click(function() {
+  console.log("Valeure de inputGroupSelect : ", $("#inputGroupSelect").val());
+  if ($("#inputGroupSelect").val() == 1) {
+    $("#mail").slideDown();
+    $("#msg").slideUp();
+    $("#appel").slideUp();
+  }else if ($("#inputGroupSelect").val() == 2) {
+    $("#mail").slideUp();
+    $("#msg").slideDown();
+    $("#appel").slideUp();
+  }else if ($("#inputGroupSelect").val() == 3) {
+    $("#mail").slideUp();
+    $("#msg").slideUp();
+    $("#appel").slideDown();
+  } else {
+    console.log("No option choosed");
+  }
 });
